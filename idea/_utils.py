d8a22c599640a66dc56b3f6cd39f500ceee6e947
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-07-14 10:29:01 -0700

    allow for centered normalization

--- idea/_utils.py
+++ idea/_utils.py
@@ -1,17 +1,22 @@
+from typing import Optional
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import to_hex
+from matplotlib.colors import CenteredNorm, to_hex
 
 
 def _array_to_hex(
     array: np.ndarray,
     palette: str = "viridis",
+    center: Optional[float] = None,
 ) -> np.ndarray:
     """
     Convert an array of values to a hex color palette.
     """
     cmap = plt.get_cmap(palette)
-    norm = plt.Normalize(array.min(), array.max())
+    if center is None:
+        norm = plt.Normalize(array.min(), array.max())
+    else:
+        norm = CenteredNorm(vcenter=center)
     rgba = cmap(norm(array))
     hex_colors = np.array([to_hex(c) for c in rgba])
     return hex_colors
